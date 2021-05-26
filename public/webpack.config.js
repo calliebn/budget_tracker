009f36c33058fefb6c745c2943c756231f176bd7
Callie Nipper <callie.nipper@gmail.com>
2021-05-26 16:59:52 -0700

    Created webpack

---
+++ public/webpack.config.js
@@ -0,0 +1,49 @@
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const path = require('path');
+
+const config = {
+    entry: {
+        app: 'server.js',
+    },
+
+    output: {
+        path: __dirname + '/dist',
+        filename: '[name].bundle.js',
+    },
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            },
+        ],
+    },
+    plugins: [
+        new WebpackPwaManifest({
+            fingerprints: false,
+            name: 'Budget app',
+            short_name: 'budget',
+            description: 'An application that allows you keep track of your finances offline.',
+            background_color: '#01579b',
+            theme_color: '#ffffff',
+            'theme-color': '#ffffff',
+            start_url: '/',
+            icons: [
+                {
+                    src: path.resolve('icons/icon-512x512.png'),
+                    sizes: [96, 128, 192, 256, 384, 512],
+                    destination: path.join('icons'),
+                },
+            ],
+        }),
+    ],
+};
+
+module.exports = config;
